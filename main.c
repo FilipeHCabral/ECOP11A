f0e655b12404b42c3d77537f47e09264151185dc
Filipe Herlon Cabral de Freitas <d2022015845@unifei.edu.br>
2024-10-22 12:41:55 +0000

    Primeira versão

--- main.c
+++ main.c
@@ -7,19 +7,18 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-// o html é no formato latino, por isso é necesário usar o charset=iso-8859-1
 const char *html_content = 
 "HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html\r\n\r\n"
-"<!DOCTYPE html charset=iso-8859-1\>"
+"Content-Type: text/html; charset=utf-8\r\n\r\n"
+"<!DOCTYPE html>"
 "<html>"
 "<head><title>Servidor Carlos</title></head>"
 "<body>"
 "<h1>Controlador de Acesso</h1>"
-"<p>Olá, seja bem-vindo ao controlador de acesso de Carlos!</p>"
+"<p>Olá, seja bem-vindo ao controlador de acesso de Filipe!</p>"
 "<p>Este é um servidor que foi criado para demonstrar o funcionamento de um servidor web.</p>"
-"<p>Este servidor foi criado por Carlos Henrique.</p>"
-"<p>Este servidor foi criado para a disciplina de Redes de Computadores.</p>"
+"<p>Este servidor foi criado por Filipe Herlon.</p>"
+"<p>Este servidor foi criado para a disciplina de Introdução à programação.</p>"
 "<p>Este servidor foi criado para a Universidade Federal de Itajuba.</p>"
 "<body>"
 "</html>";
